edbee4172576809476ec6bf5e7661dc281a3b5bd
Juanpa81 <145733669+Juanpa81@users.noreply.github.com>
2026-04-17 12:07:41 -0500

    Primer avance - clase Cliente

--- main.py
+++ main.py
@@ -14,18 +14,3 @@ class Cliente:
 cliente1 = Cliente("Juan", "123456")
 
 cliente1.mostrar_datos()
-
-
-# Clase Servicio (Clase Base)
-
-
-class Servicio:
-    def __init__(self, nombre, precio):
-        self.nombre = nombre
-        self.precio = precio
-
-    def mostrar_servicio(self):
-        print("Servicio:", self.nombre)
-        print("Precio:", self.precio)
-
-
